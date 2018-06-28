223b68980ea50406c9a2d1981aeddb0b885e0e30
c1fr1 <c1fr1.stkcps@gmail.com>
2018-06-28 07:36:56 +0100

    added support for the creation of textures which are made from byte buffers

--- src/engine/OpenGL/Texture.java
+++ src/engine/OpenGL/Texture.java
@@ -69,6 +69,38 @@ public class Texture {
 	}
 	
 	/**
+	 * creates a new texture from a buffered image
+	 * @param bi image
+	 */
+	public Texture(BufferedImage bi) {
+		width = bi.getWidth();
+		height = bi.getHeight();
+		int[] pixels = bi.getRGB(0, 0, width, height, null, 0, width);
+		ByteBuffer buffer = BufferUtils.createByteBuffer(width * height * 4);
+		
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < width; j++) {
+				int pixel = pixels[i * width + j];
+				buffer.put((byte)((pixel >> 16) & 0xFF)); // Red
+				buffer.put((byte)((pixel >>  8) & 0xFF)); // Green
+				buffer.put((byte)((pixel	  ) & 0xFF)); // Blue
+				buffer.put((byte)((pixel >> 24) & 0xFF)); // Alpha
+			}
+		}
+		buffer.flip();
+		
+		id = glGenTextures();
+		textureIDs.add(id);
+		bind();
+		glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+		glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, buffer);
+		unbind();
+	}
+	
+	/**
 	 * creates a new empty texture given a width and height
 	 * @param w width
 	 * @param h height
