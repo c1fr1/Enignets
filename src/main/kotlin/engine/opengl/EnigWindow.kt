819c9fa3317b0def8382f1691828d5e9f57d18a5
C1FR1 <c1fr1.stkcps@gmail.com>
2021-10-22 20:51:51 +0000

    updated gl version window hints

--- src/main/kotlin/engine/opengl/EnigWindow.kt
+++ src/main/kotlin/engine/opengl/EnigWindow.kt
@@ -53,8 +53,8 @@ open class EnigWindow {
 		glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE)
 		glfwWindowHint(GLFW_RESIZABLE, if (resizeable) 1 else 0)
 		glfwWindowHint(GLFW_DECORATED, if (decorated) 1 else 0)
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3)
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3)
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4)
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6)
 		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE)
 		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE)
 
