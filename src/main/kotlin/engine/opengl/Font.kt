45fdc08938506ade268fb9814764bf258ba57dbf
C1FR1 <c1fr1.stkcps@gmail.com>
2023-10-31 15:18:28 -0700

    added more options for getting font transformations

--- src/main/kotlin/engine/opengl/Font.kt
+++ src/main/kotlin/engine/opengl/Font.kt
@@ -2,18 +2,14 @@ package engine.opengl
 
 import engine.getResourceStream
 import org.joml.Matrix4f
+import org.joml.Vector3f
 import org.lwjgl.BufferUtils.createByteBuffer
 import org.lwjgl.opengl.GL11.GL_RED
 import org.lwjgl.stb.STBTTBakedChar
 import org.lwjgl.stb.STBTruetype
 import org.lwjgl.system.MemoryUtil
-import org.lwjgl.system.MemoryUtil.memSlice
-import java.io.IOException
 import java.io.InputStream
 import java.nio.ByteBuffer
-import java.nio.file.Files
-import java.nio.file.Path
-import java.nio.file.Paths
 
 
 open class Font : Texture {
@@ -44,23 +40,46 @@ open class Font : Texture {
 		}
 	}
 
-	fun getMats(text : String, base : Matrix4f, ret : (worldMats : Array<Matrix4f>, texCoordMats: Array<Matrix4f>) -> Unit) {
-
-		val worldMats = Array(text.length) {Matrix4f()}
-		val tcMats = Array(text.length) {Matrix4f()}
-
-		val b = Matrix4f(base)
+	fun getTransforms(text : String,
+	                  r : (
+		                  charTranslation : Vector3f,
+		                  charScale : Vector3f,
+		                  tcTranslation : Vector3f,
+		                  tcScale : Vector3f
+	                  ) -> Unit) {
+		var xAdv = 0f
 		for (i in text.indices) {
 			val co = charData[text[i].code - 32]
 			val cwidth = co.x1() - co.x0()
 			val cheight = co.y1() - co.y0()
-			worldMats[i] = Matrix4f(b)
-				.translate(co.xoff() / fontSize, -(co.yoff() + cheight) / fontSize, 0f)
-				.scale(cwidth / fontSize, cheight / fontSize, 1f)
-			tcMats[i] = Matrix4f().translate(co.x0() / width.toFloat(), co.y0() / height.toFloat(), 0f)
-					.scale(cwidth / width.toFloat(), cheight / height.toFloat(), 1f)
-			b.translate(co.xadvance() / fontSize, 0f, 0f)
+			val charTranslation = Vector3f(co.xoff() / fontSize + xAdv, -(co.yoff() + cheight) / fontSize, 0f)
+			val charScale = Vector3f(cwidth / fontSize, cheight / fontSize, 1f)
+			val tcTranslation = Vector3f(co.x0() / width.toFloat(), co.y0() / height.toFloat(), 0f)
+			val tcScale = Vector3f(cwidth / width.toFloat(), cheight / height.toFloat(), 1f)
+
+			r(charTranslation, charScale, tcTranslation, tcScale)
+			xAdv += co.xadvance() / fontSize
 		}
+	}
+
+	fun getMats(text : String, base : Matrix4f, r : (charMat : Matrix4f, tcMat : Matrix4f) -> Unit) {
+		getTransforms(text) {charTranslation, charScale, tcTranslation, tcScale ->
+			r(Matrix4f(base).translate(charTranslation).scale(charScale),
+				Matrix4f().translate(charTranslation).scale(tcScale))
+		}
+	}
+
+	fun getMatsArray(text : String, base : Matrix4f, ret : (worldMats : Array<Matrix4f>, texCoordMats: Array<Matrix4f>) -> Unit) {
+
+		val worldMats = Array(text.length) {Matrix4f()}
+		val tcMats = Array(text.length) {Matrix4f()}
+		var i = 0
+
+		getMats(text, base) {charMat, tcMat ->
+			worldMats[i] = charMat
+			tcMats[i++] = tcMat
+		}
+
 		ret(worldMats, tcMats)
 	}
 }
